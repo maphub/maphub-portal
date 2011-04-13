f25cafcb2c537c2ef008e4f185bf1cfb5fe99a35
Rainer Simon <rainer.simon@ait.ac.at>
2011-04-13 14:04:10 +0200

    Created Controller for Map domain class

---
+++ grails-app/controllers/at/ait/dme/maphub/MapController.groovy
@@ -0,0 +1,5 @@
+package at.ait.dme.maphub
+
+class MapController {
+    def scaffold = Map
+}
---
+++ test/unit/at/ait/dme/maphub/MapControllerTests.groovy
@@ -0,0 +1,17 @@
+package at.ait.dme.maphub
+
+import grails.test.*
+
+class MapControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
