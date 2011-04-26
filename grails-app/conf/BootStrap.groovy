7f315ca7bc558af4c91fa59100adeab6ebf16764
cmader <christian.mader@univie.ac.at>
2011-04-26 22:52:00 +0200

    removed needless role constants

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,10 +5,6 @@ import at.ait.dme.maphub.UserRole
 
 class BootStrap {
 
-	final ADMIN_ROLE_AUTHORITY = "ROLE_ADMIN"
-	final WRITE_ACCESS_USER_ROLE_AUTHORITY = "ROLE_USER_RW"
-	final READONLY_ACCESS_USER_ROLE_AUTHORITY = "ROLE_USER_RO"
-
 	final ADMIN_USER_NAME = "admin"
 	final WRITE_ACCESS_USER_NAME = "rwuser"
 	final READONLY_USER_NAME = "rouser"
@@ -32,7 +28,7 @@ class BootStrap {
 	}
 
 	void createRolesIfRequired() {
-		def roles = [ADMIN_ROLE_AUTHORITY, WRITE_ACCESS_USER_ROLE_AUTHORITY, READONLY_ACCESS_USER_ROLE_AUTHORITY]
+		def roles = ["ROLE_ADMIN", "ROLE_USER_RW", "ROLE_USER_RO"]
 		roles.each() {
 			if (!Role.findByAuthority(it)) {
 				new Role(authority: it).save(flush: true)
@@ -43,18 +39,18 @@ class BootStrap {
 	void createAdminUserIfRequired() {
 		if (!User.findByUsername(ADMIN_USER_NAME)) {
 			def admin = createUser(ADMIN_USER_NAME, ADMIN_USER_NAME)
-			UserRole.create(admin, Role.findByAuthority(ADMIN_ROLE_AUTHORITY), true)
+			UserRole.create(admin, Role.findByAuthority("ROLE_ADMIN"), true)
 		}
 	}
 
 	void createAverageJoeUsersIfRequired() {
 		if (!User.findByUsername(WRITE_ACCESS_USER_NAME)) {
 			def uploadUser = createUser(WRITE_ACCESS_USER_NAME, WRITE_ACCESS_USER_NAME)
-			UserRole.create(uploadUser, Role.findByAuthority(WRITE_ACCESS_USER_ROLE_AUTHORITY), true)
+			UserRole.create(uploadUser, Role.findByAuthority("ROLE_USER_RW"), true)
 		}
 		if (!User.findByUsername(READONLY_USER_NAME)) {
 			def readOnlyUser = createUser(READONLY_USER_NAME, READONLY_USER_NAME)
-			UserRole.create(readOnlyUser, Role.findByAuthority(READONLY_ACCESS_USER_ROLE_AUTHORITY), true)
+			UserRole.create(readOnlyUser, Role.findByAuthority("ROLE_USER_RO"), true)
 		}
 	}
 
