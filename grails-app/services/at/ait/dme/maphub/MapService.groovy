03539348d371845f698ef5f1fb32cdf74e3f8fd3
cmader <christian.mader@univie.ac.at>
2011-06-15 14:46:35 +0200

    added correct URI generation for OAC serialization

--- grails-app/services/at/ait/dme/maphub/MapService.groovy
+++ grails-app/services/at/ait/dme/maphub/MapService.groovy
@@ -7,19 +7,26 @@ import at.ait.dme.yuma.server.formathandler.rdf.oac.OACFormatHandler
 
 class MapService {
   
+	def grailsApplication
+
 	String serializeOAC(at.ait.dme.maphub.Annotation annotationEntity) {
 		String annotationId = annotationEntity.id.toString()
 		User user = new User(annotationEntity.user.username)
 		String objectUri = findAnnotatedMap(annotationEntity).tilesetUrl
+		String annotationUri = getAnnotationUri(annotationEntity)
 
-		// TODO: replace hardcoded annotation uri by annotation controller path
-		Annotation annotation = new Annotation("http://some.annotation.com/1", user, MediaType.IMAGE)
+		Annotation annotation = new Annotation(annotationUri, user, MediaType.IMAGE)
 		annotation.addObjectUri(objectUri)
 		return new OACFormatHandler().serialize(annotation)
 	}
 
 	Map findAnnotatedMap(at.ait.dme.maphub.Annotation annotationEntity) {
 		return Map.findAllByAnnotations(annotationEntity)
+	}
+
+	String getAnnotationUri(at.ait.dme.maphub.Annotation annotationEntity) {
+		def g = grailsApplication.mainContext.getBean('org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib')
+		g.createLink(controller:"Map", action: "show", id: annotationEntity.id, absolute: true)
 	}
 
 }
