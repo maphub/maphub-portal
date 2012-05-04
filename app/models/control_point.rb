99038067f97fcf97f0c5720f21b14fa44f267c16
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-05-04 12:12:50 -0400

    implemented coordinate transformation functions

--- app/models/control_point.rb
+++ app/models/control_point.rb
@@ -1,6 +1,10 @@
 class ControlPoint < ActiveRecord::Base
+  
   belongs_to :user, :counter_cache => true
   belongs_to :map
+  
+  validates_presence_of :geonames_id, :lat, :lng, :x, :y
+  
   
   # The Geonames RDF URI for this place
   def geonames_uri
@@ -9,6 +13,98 @@ class ControlPoint < ActiveRecord::Base
   
   def fragment_uri
     self.map.raw_image_uri + "#xywh=#{self.x.to_i},#{self.y.to_i},1,1"
+  end
+  
+  
+  # LAT/LON and X/Y are assumed to be related via an affine transform.
+  # An affine transform takes the form
+  # 
+  #   x = a * u + b * v + c
+  #   y = d * u + e * v + f
+  # 
+  # where x and y are the point's unknown X/Y coordinates (image space);
+  # and u and v are the point's known geographical coordinates (u = lat,
+  # v = lon). Coefficients a, b, c, d, e, f can be computed if three
+  # control points are known (in image space and geo-coordinate space).
+  # 
+  # The known coordinates of the three control points are denoted as
+  # 
+  # CP1: x1/y1 (image space) u1/v1 (lat/lon)
+  # CP2: x2/y2               u2/v2
+  # CP3: x3/y3               u3/v3 
+  def self.compute_xy_from_known_latlng(lat, lng, control_points)
+    raise ArgumentError if control_points.length != 3
+    
+    # Compute matrix coefficients
+    x1 = control_points[0].x;
+    y1 = control_points[0].y;
+    x2 = control_points[1].x;
+    y2 = control_points[1].y;
+    x3 = control_points[2].x;
+    y3 = control_points[2].y;
+
+    u1 = control_points[0].lat;
+    v1 = control_points[0].lng;
+    u2 = control_points[1].lat;
+    v2 = control_points[1].lng;
+    u3 = control_points[2].lat;
+    v3 = control_points[2].lng;
+
+    a = (x2 * (v3 - v1) - x3 * (v2 - v1) - x1 * (v3 - v2))/
+        ((u2 - u1) * (v3 - v1) - (u3 - u1) * (v2 - v1));
+    b = (x2 * (u3 - u1) - x3 * (u2 - u1) - x1 * (u3 - u2))/
+        ((v2 - v1) * (u3 - u1) - (v3 - v1) * (u2 - u1));
+    c = x1 - a * u1 - b * v1;
+
+    d = (y2 * (v3 - v1) - y3 * (v2 - v1) - y1 * (v3 - v2))/
+        ((u2 - u1) * (v3 - v1) - (u3 - u1) * (v2 - v1));
+    e = (y2 * (u3 - u1) - y3 * (u2 - u1) - y1 * (u3 - u2))/
+        ((v2 - v1) * (u3 - u1) - (v3 - v1) * (u2 - u1));
+    f = y1 - d * u1 - e * v1;
+
+    # Compute transform
+    x = a * lat + b * lng + c;
+    y = d * lat + e * lng + f;
+
+    return x, y
+  end
+  
+  
+  def self.compute_latlng_from_known_xy(x, y, control_points)
+      raise ArgumentError if control_points.length != 3
+
+  		# Compute matrix coefficients
+  		x1 = control_points[0].x;
+  		y1 = control_points[0].y;
+  		x2 = control_points[1].x;
+  		y2 = control_points[1].y;
+  		x3 = control_points[2].x;
+  		y3 = control_points[2].y;
+
+  		u1 = control_points[0].lat; 
+  		v1 = control_points[0].lng;
+  		u2 = control_points[1].lat;
+  		v2 = control_points[1].lng;
+  		u3 = control_points[2].lat;
+  		v3 = control_points[2].lng;
+
+  		a = (u2 * (y3 - y1) - u3 * (y2 - y1) - u1 * (y3 - y2))/
+  		    ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));		
+  		b = (u2 * (x3 - x1) - u3 * (x2 - x1) - u1 * (x3 - x2))/
+  		    ((y2 - y1) * (x3 - x1) - (y3 - y1) * (x2 - x1));
+  		c = u1 - a * x1 - b * y1;
+
+  		d = (v2 * (y3 - y1) - v3 * (y2 - y1) - v1 * (y3 - y2))/
+  		    ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+  		e = (v2 * (x3 - x1) - v3 * (x2 - x1) - v1 * (x3 - x2))/
+  		    ((y2 - y1) * (x3 - x1) - (y3 - y1) * (x2 - x1));			
+  		f = v1 - d * x1 - e * y1;
+
+  		# Compute transform
+  		u = a * x + b * y + c;
+  		v = d * x + e * y + f;
+
+  		return u,v
   end
   
   
--- test/unit/control_point_test.rb
+++ test/unit/control_point_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ControlPointTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "control point count" do
+    assert_equal 3, ControlPoint.count
+  end
+  
+  test "getting X and Y from known Lat and Lng" do
+    
+    control_points = ControlPoint.all
+
+    assert_equal(
+      ControlPoint.
+        compute_xy_from_known_latlng(-49.4369173974526, 42.33239970898038, 
+          control_points).collect! {|x| x.to_i}, [617, 404])
+    
+    assert_equal(
+      ControlPoint.
+        compute_xy_from_known_latlng(-74.98360209681323, 71.91052729045569,
+          control_points).collect! {|x| x.to_i}, [699, 484])
+    
+    assert_equal(
+      ControlPoint.
+        compute_xy_from_known_latlng(-32.01647405811647, 119.43088240522445,
+          control_points).collect! {|x| x.to_i}, [831, 350])
+        
+  end
+
+  test "getting Lat and Lng from known X and Y" do
+    
+    control_points = ControlPoint.all
+
+    lat, lng = ControlPoint.
+                compute_latlng_from_known_xy(617, 404, control_points)
+    assert_in_delta(lat, -49.4369173974526, 0.5)
+    assert_in_delta(lng, 42.33239970898038, 0.5)
+        
+  end
+
 end
