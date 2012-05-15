3d6f20cdf02e76a4a4d194700aca33f714c7a2df
Josh <josh@endries.org>
2012-05-14 20:04:32 -0400

    Added support for optional mobile views when using an "m" subdomain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout "default" # <= Use twitter-bootstrap layout called "default" per default
-  
+
+
+
+  #
+  # Optional mobile views based on:
+  # http://erniemiller.org/2011/01/05/mobile-devices-and-rails-maintaining-your-sanity/
+  #  
+  before_filter :prepend_mobile_path
+  def prepend_mobile_path() if mobile_request? then prepend_view_path Rails.root+'app'+'mobile_views' end end
+  def mobile_request?() request.subdomains.first == 'm' end
+  helper_method :mobile_request?
+
+
   
   # Cuts a possible filename from the given URI
   # => http://localhost:3000/maps/1.rdf => http://localhost:3000/maps/1
@@ -12,5 +24,4 @@ class ApplicationController < ActionController::Base
     end
     uri
   end
-  
 end
---
+++ app/mobile_views/README.md
@@ -0,0 +1,3 @@
+# Mobile Views
+
+Views in this directory are used only if the client is accessing the site via a m.domain.tld domain name (i.e., they are using the 'm' subdomain).
