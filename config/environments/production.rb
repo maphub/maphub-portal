621787d7b04ea6510fe8399ffafd20f186377e83
slhck <werner.robitza@gmail.com>
2012-07-12 20:04:34 -0400

    Fixed paths to assets for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,16 +18,19 @@ MaphubPortal::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  # Change prefix so OpenLayers images load correctly
+  config.assets.prefix = "/"
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
