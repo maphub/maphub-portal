2eb8d142d6c2fb7a6e1eb95396d7ae33cdbe3c9e
slhck <werner.robitza@gmail.com>
2012-07-18 11:27:47 -0400

    Added pagination to search, minor view improvements / fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,38 +6,19 @@ class HomeController < ApplicationController
 
   def search
     
-    @maps = []
     @q = params[:q]
     
     @search = Map.search do
       keywords params[:q]
     end
+    
     @results = @search.results
     
-    # @results = []
-    # @q = params[:q]
-    # 
-    # unless params[:q].nil?
-    #   maps = Map.search do
-    #     keywords params[:q]
-    #   end.results
-    #   
-    #   annotations = Annotation.search do
-    #     keywords params[:q]
-    #  end.results
-    #   
-    #   
-    #   
-    #   # get the maps from the annotations and tags and put them into one array
-    #   annotation_maps = annotations.collect { |a| a.map }
-    #   
-    #   @results = maps.concat(annotation_maps).uniq
-    # #  @results = maps.concat(tag_maps).uniq
-    # end
+    @results = Kaminari.paginate_array(@results).page(params[:page]).per(20)
     
     respond_to do |format|
       format.html # search.html.erb
-      format.xml  { render :xml => @maps }
+      format.xml  { render :xml => @results }
     end
     
   end
