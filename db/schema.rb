6d35e643c39124dc06e1eba2685789112487a7a1
Keith Newman <kin7@cornell.edu>
2012-08-16 16:33:01 -0400

    Updating experiment branch

--- db/schema.rb
+++ db/schema.rb
@@ -74,11 +74,11 @@ ActiveRecord::Schema.define(:version => 20120801211828) do
 
   create_table "maps", :force => true do |t|
     t.string   "identifier"
-    t.string   "title"
-    t.string   "subject"
+    t.text   "title"
+    t.text   "subject"
     t.integer  "width"
     t.integer  "height"
-    t.string   "author"
+    t.text   "author"
     t.string   "date"
     t.boolean  "overlay_available"
     t.datetime "created_at",        :null => false
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(:version => 20120801211828) do
     t.integer  "control_points_count"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "condition_assignment"
+    t.text   "condition_assignment"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
